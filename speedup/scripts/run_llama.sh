3ae43beefa6ea82339c1f740804b6ca506c96808
we1pingyu <we1pingyu>
2024-12-23 16:36:05 +0800

    fix weight percent

--- speedup/scripts/run_llama.sh
+++ speedup/scripts/run_llama.sh
@@ -5,6 +5,6 @@ MODEL="meta-llama/Llama-2-13b-hf"
 # for MODEL in "meta-llama/Llama-3.1-70B-Instruct" 
 # for MODEL in "facebook/opt-6.7B"
 CMD="--model $MODEL"
-CMD=$CMD" --percent 20 80 50 50 100 0"
+CMD=$CMD" --percent 20 80 0 100 100 0"
 CMD=$CMD" --gpu-batch-size 8 --num-gpu-batches 8 --prompt-len 512 --gen-len 32 --computation-policy default --overlap False --cpu-cache-compute" 
 python -m flexgen.flex_llama $CMD
